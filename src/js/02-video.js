ab90829a8ca2887bfa3aa29028a8338da394eb69
beligrous <beligrous@gmail.com>
2022-11-11 20:07:57 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,37 @@
 import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
+const player = new Vimeo(iframe);
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
 const onPlay = function (data) {
-  // data is an object containing properties specific to that event
+  let timing = localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
 };
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let seconds = localStorage.getItem('videoplayer - current - time');
+
+player
+  .setCurrentTime()
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
